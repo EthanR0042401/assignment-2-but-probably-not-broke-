e671def09d1de92e8be098a1b539691c5959dab7
Ethan Christopher Thompson <RC0042401@student.riversidecollege.ac.uk>
2020-11-02 15:55:21 +0000

    Add project files.

---
+++ assignment 2 (but probably not broke)/UserControl1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace assignment_2__but_probably_not_broke_
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double length = double.Parse(textBox1.Text);
+                double width = double.Parse(textBox2.Text);
+                double height = double.Parse(textBox3.Text);
+                
+            }
+        }
+
+
+    }
+}   
